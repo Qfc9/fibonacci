cc2880e0b900889ff679f2c8d082f81257927c2b
Elijah Harmon <elijahharmon@gmail.com>
2018-05-03 15:01:22 -0400

    Basic argc checker

--- main.s
+++ main.s
@@ -1,6 +1,19 @@
 .intel_syntax noprefix
 
+invalid_arg_msg:
+    .asciz "Invalid amount of args!\n"
+
+invalid_args:
+    mov     edi, OFFSET invalid_arg_msg
+    call puts
+    jmp end
+
 .globl main
 main:
-    xor eax, eax
+    xor     eax, eax
+    mov     edx, 2
+    cmp     edx, edi
+    jne     invalid_args
+
+end:
     ret
