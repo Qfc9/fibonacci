520167fa029bedcdb798e7ac655be31cfb98105a
Elijah Harmon <elijahharmon@gmail.com>
2018-05-05 11:27:58 -0400

    Added a testing shell

---
+++ test_cases.sh
@@ -0,0 +1,11 @@
+make debug
+clear
+./fibonacci
+./fibonacci 0
+./fibonacci -o 0
+./fibonacci 100
+./fibonacci -o 100
+./fibonacci 10 10
+./fibonacci -f 10
+./fibonacci 10 10 10
+./fibonacci -o 10 10
